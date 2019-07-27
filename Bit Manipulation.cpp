be7385f4dde62a8e2dde90b497caae1d053151e6
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-07-27 12:18:29 +0530

    Delete Bit Manipulation.cpp

--- Bit Manipulation.cpp
+++
@@ -1,5 +0,0 @@
-int Solution::numSetBits(unsigned int A) {
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    // Using builtin functions always serves you giving an upper edge
-    return __builtin_popcount(A);
-}
